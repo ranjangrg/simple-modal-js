aeda43982b0a5e1b1b747c6fc52aa426a5104019
ranjangrg <32595621+ranjangrg@users.noreply.github.com>
2022-09-18 12:32:37 +0100

    Added typescript src files

--- modal.ts
+++ modal.ts
@@ -1,7 +1,17 @@
-var ModalController = /** @class */ (function () {
-    function ModalController(props) {
-        this.closeSymbolDim = 1 * this.getDocumentRootFontSizeInPixels(); // 1rem
-        this.closeSymbol = "<svg xmlns=\"http://www.w3.org/2000/svg\" \n        width=\"".concat(this.closeSymbolDim, "\" height=\"").concat(this.closeSymbolDim, "\" \n        viewBox=\"0 0 ").concat(this.closeSymbolDim, " ").concat(this.closeSymbolDim, "\" \n        overflow=\"visible\" stroke=\"white\" stroke-width=\"5\" stroke-linecap=\"round\">\n        <line x2=\"").concat(this.closeSymbolDim, "\" y2=\"").concat(this.closeSymbolDim, "\" />\n        <line x1=\"").concat(this.closeSymbolDim, "\" y2=\"").concat(this.closeSymbolDim, "\" />\n    </svg>");
+class ModalController {
+    closeSymbolDim = 1 * this.getDocumentRootFontSizeInPixels();    // 1rem
+    modalBtnElem: HTMLElement;
+    modalContent: string;
+    modalTitle: string;
+    modalContentElem: HTMLElement;
+    closeSymbol = `<svg xmlns="http://www.w3.org/2000/svg" 
+        width="${this.closeSymbolDim}" height="${this.closeSymbolDim}" 
+        viewBox="0 0 ${this.closeSymbolDim} ${this.closeSymbolDim}" 
+        overflow="visible" stroke="white" stroke-width="5" stroke-linecap="round">
+        <line x2="${this.closeSymbolDim}" y2="${this.closeSymbolDim}" />
+        <line x1="${this.closeSymbolDim}" y2="${this.closeSymbolDim}" />
+    </svg>`;
+    constructor(props) {
         this.modalBtnElem = document.getElementById(props.modalBtnElemId);
         this.modalContent = props.modalContent;
         this.modalTitle = props.modalTitle;
@@ -11,57 +21,64 @@ var ModalController = /** @class */ (function () {
         this.initEventTriggers();
         this.trapFocusWithinModal();
     }
-    ModalController.prototype.getDocumentRootFontSizeInPixels = function () {
+    getDocumentRootFontSizeInPixels() {
         // basic conversion from 1rem to px: Note: no null checks in place currently
-        return parseFloat(getComputedStyle(document.querySelector('html')).fontSize.replace("px", ""));
-    };
-    ModalController.prototype.createModalElement = function () {
+        return parseFloat(getComputedStyle(document.querySelector('html')).fontSize.replace("px",""));
+    }
+    createModalElement() {
         // main modal content container
-        var modalContentContainerElem = document.createElement("div");
+        let modalContentContainerElem = document.createElement("div");
         modalContentContainerElem.setAttribute("id", this.modalBtnElem.id + "-modal-container");
         modalContentContainerElem.setAttribute("class", "modal-container");
         modalContentContainerElem.setAttribute("role", "dialog");
         modalContentContainerElem.setAttribute("aria-modal", "true");
         modalContentContainerElem.setAttribute("aria-expanded", "false");
         modalContentContainerElem.setAttribute("aria-hidden", "true");
+
         // close modal btn
-        var modalCloseBtnElem = document.createElement("button");
+        let modalCloseBtnElem = document.createElement("button");
         modalCloseBtnElem.setAttribute("class", "close-modal-btn");
         modalCloseBtnElem.setAttribute("aria-label", "Close this modal/dialog");
         modalCloseBtnElem.innerHTML = this.closeSymbol;
+
         // close modal btn container
-        var modalCloseBtnContainerElem = document.createElement("div");
+        let modalCloseBtnContainerElem = document.createElement("div");
         modalCloseBtnContainerElem.setAttribute("class", "close-modal-container");
         modalCloseBtnContainerElem.append(modalCloseBtnElem);
         // close modal btn label
-        var modalCloseBtnLabelElem = document.createElement("div");
+        let modalCloseBtnLabelElem = document.createElement("div");
         modalCloseBtnLabelElem.setAttribute("class", "close-modal-btn-label");
         modalCloseBtnLabelElem.setAttribute("aria-hidden", "true");
         modalCloseBtnLabelElem.textContent = "Close modal";
         modalCloseBtnContainerElem.append(modalCloseBtnLabelElem);
+
         // main modal content
-        var modalContentElem = document.createElement("div");
+        let modalContentElem = document.createElement("div");
         modalContentElem.setAttribute("class", "modal-content");
         modalContentElem.innerHTML = this.modalContent;
+
         // main modal title
-        var modalTitleElem = document.createElement("div");
+        let modalTitleElem = document.createElement("div");
         modalTitleElem.setAttribute("class", "modal-title");
         modalTitleElem.textContent = this.modalTitle;
+
         // main modal content wrapper
-        var modalContentWrapperElem = document.createElement("div");
+        let modalContentWrapperElem = document.createElement("div");
         modalContentWrapperElem.setAttribute("class", "modal-content-wrapper");
         modalContentWrapperElem.append(modalTitleElem);
         modalContentWrapperElem.append(modalContentElem);
+
         // add all elements next to the open modal btn in DOM
         modalContentContainerElem.append(modalCloseBtnContainerElem);
         modalContentContainerElem.append(modalContentWrapperElem);
         this.modalContentElem = modalContentContainerElem;
-    };
-    ModalController.prototype.attachModalContent = function () {
-        this.modalBtnElem.parentNode.insertBefore(this.modalContentElem, this.modalBtnElem.nextSibling);
-    };
-    ModalController.prototype.initEventTriggers = function () {
-        var mainObj = this;
+    }
+    attachModalContent() {
+        this.modalBtnElem.parentNode.insertBefore(
+            this.modalContentElem, this.modalBtnElem.nextSibling);
+    }
+    initEventTriggers() {
+        let mainObj = this;
         // click on background to close modal
         this.modalContentElem.addEventListener("click", function (e) {
             if (e.srcElement === mainObj.modalContentElem) {
@@ -69,56 +86,59 @@ var ModalController = /** @class */ (function () {
                 mainObj.closeModal(mainObj.modalContentElem);
             }
         }.bind(this.modalContentElem));
+
         // open modal
         this.modalBtnElem.addEventListener("click", function () {
             mainObj.openModal();
         }.bind(mainObj));
+
         // close modal 
-        var closeModalElems = this.modalContentElem.getElementsByClassName("close-modal-container");
-        for (var idx = 0; idx < closeModalElems.length; ++idx) {
-            var elem = closeModalElems.item(idx);
+        let closeModalElems = this.modalContentElem.getElementsByClassName("close-modal-container");
+        for (let idx = 0; idx < closeModalElems.length; ++idx) {
+            let elem = closeModalElems.item(idx);
             elem.addEventListener("click", function (e) {
                 mainObj.closeModal(mainObj.modalContentElem);
             }.bind(mainObj));
-        }
-        ;
-    };
-    ModalController.prototype.closeModal = function (modalElem) {
+        };
+    }
+    closeModal(modalElem) {
         modalElem.setAttribute("aria-expanded", "false");
         modalElem.setAttribute("aria-hidden", "true");
         modalElem.classList.remove("show");
-    };
-    ModalController.prototype.openModal = function () {
+    }
+    openModal() {
         this.modalContentElem.setAttribute("aria-expanded", "true");
         this.modalContentElem.setAttribute("aria-hidden", "false");
         // this.modalContentElem.setAttribute("tabIndex", "0");
         this.modalContentElem.classList.add("show");
-    };
-    ModalController.prototype.trapFocusWithinModal = function () {
-        var focusableElements = "button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])";
-        var firstFocusableElement = this.modalContentElem.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
-        var focusableContent = this.modalContentElem.querySelectorAll(focusableElements);
-        var lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+    }
+    trapFocusWithinModal() {
+        const focusableElements = "button, [href], input, select, textarea, [tabindex]:not([tabindex='-1'])";
+
+        const firstFocusableElement = <HTMLElement>this.modalContentElem.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+        const focusableContent = this.modalContentElem.querySelectorAll(focusableElements);
+        const lastFocusableElement = <HTMLElement>focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
         this.modalContentElem.addEventListener('keydown', function (e) {
-            var isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+            let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
             if (!isTabPressed) {
                 return;
             }
+
             if (e.shiftKey) { // if shift key pressed for shift + tab combination
                 if (document.activeElement === firstFocusableElement) {
                     lastFocusableElement.focus(); // add focus for the last focusable element
                     e.preventDefault();
                 }
-            }
-            else { // if tab key is pressed
+            } else { // if tab key is pressed
                 if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
                     firstFocusableElement.focus(); // add focus for the first focusable element
                     e.preventDefault();
                 }
             }
         });
-    };
-    return ModalController;
-}());
+    }
+}
+
 // END OF FILE 
-//# sourceMappingURL=modal.js.map
